edd48c822ea64107920c5a3a21d7fae7e861cef6
azegha <loicanicetazegha@gmail.com>
2024-01-06 18:59:40 +0100

    jkk

---
+++ src/main/java/com/example/lovethyme/LovethymeApplication.java
@@ -0,0 +1,13 @@
+package com.example.lovethyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovethymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovethymeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lovethyme/LovethymeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lovethyme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LovethymeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
