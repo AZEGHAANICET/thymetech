90e8f83027a6d24c1f1ae7a5505a267e24a0fab3
azegha <loicanicetazegha@gmail.com>
2024-01-07 23:41:31 +0100

    add features

--- src/main/java/com/example/lovethyme/controller/StudentController.java
+++ src/main/java/com/example/lovethyme/controller/StudentController.java
@@ -1,14 +1,23 @@
 package com.example.lovethyme.controller;
 
 import com.example.lovethyme.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    private List<String> countries;
+    @Value("${systems}")
+private  List<String> systems;
+    @Value("${languages}")
+    private List<String> languages;
 
     @GetMapping("/showStudentForm")
     public String showForm(Model model){
@@ -19,9 +28,14 @@ public class StudentController {
 
         // add student model to the model
         model.addAttribute("student", student);
-
+        model.addAttribute("countries", countries);
+        model.addAttribute("languages", languages);
+        model.addAttribute("systems", systems);
         return "student-form";
     }
+
+
+
     @PostMapping("/processStudentForm")
     public String processForm(@ModelAttribute("student") Student theStudent){
         System.out.println("the student : "+theStudent.getFirstName()+" "+theStudent.getLastName());
--- src/main/java/com/example/lovethyme/model/Student.java
+++ src/main/java/com/example/lovethyme/model/Student.java
@@ -1,10 +1,46 @@
 package com.example.lovethyme.model;
 
-public class Student {
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-    private String firstName;
+import java.util.List;
+
+public class Student {
+@NotNull(message = "is required please")
+@Size(min=1, message = "is required")
+    private String firstName="";
+
 
     private String lastName;
+private String country;
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    private String favoriteLanguage;
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+
+    private List<String> favoriteSystems;
+
+    public List<String> getFavoriteSystems() {
+        return favoriteSystems;
+    }
+
+    public void setFavoriteSystems(List<String> favoriteSystems) {
+        this.favoriteSystems = favoriteSystems;
+    }
+
+    public String getCountry() {
+        return country;
+    }
 
     public Student(){
 
